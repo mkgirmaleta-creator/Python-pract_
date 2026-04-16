6c2f99d3f0bd9aa5ba2806c89944cf1cf49d0fc2
Tolesa Girma Leta <mkgirmaleta@gmail.com>
2026-04-16 13:58:30 -0700

    Time: 16 ms (68.15%) | Memory: 19.3 MB (61.34%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.py
@@ -0,0 +1,29 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        if len(s1) > len(s2):
+            return False
+
+        count1 = [0] * 26
+        count2 = [0] * 26
+
+        # Fill count1
+        for c in s1:
+            count1[ord(c) - ord('a')] += 1
+
+        left = 0
+
+        for right in range(len(s2)):
+            # Add current char to window
+            count2[ord(s2[right]) - ord('a')] += 1
+
+            # If window size > len(s1), shrink
+            if right - left + 1 > len(s1):
+                count2[ord(s2[left]) - ord('a')] -= 1
+                left += 1
+
+            # Compare
+            if count1 == count2:
+                return True
+
+        return False
